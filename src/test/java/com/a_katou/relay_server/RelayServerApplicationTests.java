724379b27e4df1ade9133ffe8792c556660f23c1
a-katou <akira.x.kato@gmail.com>
2024-02-23 17:14:15 +0900

    Spring InitializrからSpring Bootプロジェクト作成

---
+++ src/test/java/com/a_katou/relay_server/RelayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a_katou.relay_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
